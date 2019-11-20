9cd8a574bae1289b57da4a09f6d44bb8ece6bbdd
João Marques <joaomarques@MBP-do-Marques.lan>
2019-11-20 22:07:13 +0000

    corrigido return no run.c

--- parallel/run.c
+++ parallel/run.c
@@ -2,21 +2,17 @@
 #include <stdlib.h>
 
 int main() {
-	char command[100], img1[100], /*img2[100],*/ img3[100], img4[100];
+	char command[100], img1[100], img2[100], img3[100];
 	for(int i = 2 ; i <= 40 ; i++) {
 		sprintf(command,"export OMP_NUM_THREADS=%d",i);
 		system(command);
 		sprintf(img1,"./exec imgTeste/gun.pbm imgResult/gun%d.pbm times/timegun2.txt",i);
 		system(img1);
-		printf("Gun number %d finished\n",i);
-		//sprintf(img2,"./exec imgTeste/flowers.pbm imgResult/flowers%d.pbm times/timeflowers2.txt",i);
-		//system(img2);
-		sprintf(img3,"./exec imgTeste/digital.pbm imgResult/digital%d.pbm times/timedigital2.txt",i);
+		sprintf(img2,"./exec imgTeste/digital.pbm imgResult/digital%d.pbm times/timedigital2.txt",i);
+		system(img2);
+		sprintf(img3,"./exec imgTeste/exemplo.pbm imgResult/exemplo%d.pbm times/timeexemplo2.txt",i);
 		system(img3);
-		printf("Digital number %d finished\n",i);
-		sprintf(img4,"./exec imgTeste/exemplo.pbm imgResult/exemplo%d.pbm times/timeexemplo2.txt",i);
-		system(img4);
-		printf("Exemplo number %d finished\n",i);
+		printf("Iteração %d\n",i);
 	}
-	return 1;
+	return 0;
 }
