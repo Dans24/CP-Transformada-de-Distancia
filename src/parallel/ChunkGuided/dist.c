ea6d08fa65a1d3c6c28b1717e5a44dee1d7a1e4a
Daniel Costa <daniel.costa1998@gmail.com>
2019-12-10 21:25:50 +0000

    Update dist.c

--- src/parallel/ChunkGuided/dist.c
+++ src/parallel/ChunkGuided/dist.c
@@ -37,12 +37,12 @@ int main(int argc, char* argv[]) {
 unsigned int dist(unsigned int height, unsigned int width, pixel (*img)[width], pixel (**output)[width]) {
     pixel (*aux)[width] = calloc(1, sizeof(pixel[height][width]));
     int iter;
-    int whitePixel = 1; // inicializado a 1 para entrar no for loop
+    int whitePixel = 1;
     int chunkHeight = 256;
     int chunkWidth = 2048;
     int ni = (height-2)/chunkHeight + 1;
     int nj = (width-2)/chunkWidth + 1;
-    for (iter = 1; iter < MAX_PIXEL_VALUE && whitePixel; iter++) { // trocar por um "do while"?
+    for (iter = 1; iter < MAX_PIXEL_VALUE && whitePixel; iter++) {
         #pragma omp parallel
         {
             whitePixel = 0;
@@ -76,7 +76,7 @@ int distChunk(unsigned int ii, unsigned int jj, unsigned int height, unsigned in
     int whitePixel = 0;
     for (int i = ii; i < height + ii; i++) {
         for (int j = jj; j < width + jj; j++) {
-            if (in[i][j] == MIN_PIXEL_VALUE) continue; // avança pixeis pretos
+            if (in[i][j] == MIN_PIXEL_VALUE) continue;
             pixel min = MAX_PIXEL_VALUE;
             for (int ki = -1; ki <= 1; ki++) {
                 for (int kj = -1; kj <= 1; kj++) {
